392ab6a8d07464a383167c5d4d62c4c2c824db77
SpawnTerror <I$kierka85>
2020-11-26 19:30:24 +0000

    ready

--- check_stock.py
+++ check_stock.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from selenium import webdriver
 import platform
 import os
--- email_me.py
+++ email_me.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import config
 import smtplib
 from email.mime.text import MIMEText
